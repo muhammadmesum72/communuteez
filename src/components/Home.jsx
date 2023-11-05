d72eca906c4cf2eafba392c4af2f94b9b82a8299
muhammadmesum72 <me.m.mesum@gmail.com>
2023-11-05 18:19:01 +0500

    Added Discord Link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
               <a href="https://twitter.com/communiteez" target="_blank">
                 <img src={Twitter} alt="Twitter" className="w-7 md:w-16 " />
               </a>
-              <a href="#" target="_blank">
+              <a href="https://discord.com/invite/C2zGrTSyM7" target="_blank">
                 <img src={Discord} alt="Discord" className="w-7 md:w-16 " />
               </a>
             </div>
